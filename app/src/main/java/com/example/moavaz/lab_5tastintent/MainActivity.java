5f16e531aba0e6d65ee126ee2f172cad3f08a10a
Moavaz <ezioauditore144@gmail.com>
2017-07-12 18:06:43 +0500

    Initial commit

---
+++ app/src/main/java/com/example/moavaz/lab_5tastintent/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.moavaz.lab_5tastintent;
+
+import android.content.Intent;
+import android.preference.EditTextPreference;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.btn);
+        final EditText editText = (EditText) findViewById(R.id.edt);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("edt text", editText.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+}
